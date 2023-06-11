f03a64fe18ea23915c4a9971775416a1d0ef225d
Muhammad Azam Azman <128273180+azamazman96@users.noreply.github.com>
2023-06-11 11:36:55 +0800

    dbmysql

--- exercise/php/azamazman96/db2.php
+++ exercise/php/azamazman96/db2.php
@@ -1,5 +1,6 @@
 <?php
-
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
 $link = mysqli_connect("localhost", "root", "");
  
 // Check connection
@@ -7,9 +8,9 @@ if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
  
-
 // Print host information
 echo "Connect Successfully. Host info: " . mysqli_get_host_info($link);
-
+ 
+// Close connection
+mysqli_close($link);
 ?>
-
