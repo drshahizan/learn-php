226ea95f19caa8f0d0d2875dc349e6b766dac401
Dzin Syafika <128202464+dzinsyafika97@users.noreply.github.com>
2023-06-21 19:50:45 +0800

    Create project.html

---
+++ project/2-bootstrap/submission/dzinsyafika97/js/scripts.js
@@ -0,0 +1,8 @@
+/*!
+* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
