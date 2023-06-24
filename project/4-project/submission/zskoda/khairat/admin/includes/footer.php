47795b18f70256cd589c91ad9235de4b34d9b242
MOHD NOOR HAFIZ BIN MUHAMAD SAID <128025698+mohdnoorhafiz@users.noreply.github.com>
2023-06-24 14:30:27 +0800

    admin

---
+++ project/4-project/submission/zskoda/khairat/admin/includes/footer.php
@@ -0,0 +1,3 @@
+<div class="copyrights">
+	 <p>© 2023 Sistem Pengurusan Surau Al-ansar  <a href="#">SPSA</a> </p>
+</div>	
---
+++ project/4-project/submission/zskoda/khairat/admin/includes/sidebarmenu.php
@@ -0,0 +1,40 @@
+<div class="sidebar-menu">
+	<header class="logo1">
+		<a href="#" class="sidebar-icon"> <span class="fa fa-bars"></span> </a>
+	</header>
+	<div style="border-top:1px ridge rgba(255, 255, 255, 0.15)"></div>
+	<div class="menu">
+		<ul id="menu">
+			<li><a href="dashboard.php"><i class="fa fa-tachometer"></i> <span>Paparan Utama</span>
+					<div class="clearfix"></div>
+				</a></li>
+
+			<li id="menu-academico"><a href="#"><i class="fa fa-list-ul" aria-hidden="true"></i><span> Pakej Khairat</span> <span class="fa fa-angle-right" style="float: right"></span>
+					<div class="clearfix"></div>
+				</a>
+				<ul id="menu-academico-sub">
+					<!--<li id="menu-academico-avaliacoes"><a href="create-package.php">Create</a></li>-->
+					<li id="menu-academico-avaliacoes"><a href="manage-packages.php">Manage</a></li>
+				</ul>
+			</li>
+			<li id="menu-academico"><a href="manage-users.php"><i class="fa fa-users" aria-hidden="true"></i><span>Urus Ahli</span>
+					<div class="clearfix"></div>
+				</a></li>
+
+			<li><a href="manage-bookings.php"><i class="fa fa-list" aria-hidden="true"></i> <span>Urus Tempahan</span>
+					<div class="clearfix"></div>
+				</a></li>
+			<li><a href="manageissues.php"><i class="fa fa-table"></i> <span>Urus isu</span>
+					<div class="clearfix"></div>
+				</a></li>
+			<li><a href="manage-enquires.php"><i class="fa fa-file-text-o" aria-hidden="true"></i> <span>Urus Pertanyaan</span>
+					<div class="clearfix"></div>
+				</a></li>
+			<!--<li><a href="manage-pages.php"><i class="fa fa-file-text-o" aria-hidden="true"></i> <span>Manage Pages</span>
+					<div class="clearfix"></div>
+				</a></li>-->
+
+
+		</ul>
+	</div>
+</div>
