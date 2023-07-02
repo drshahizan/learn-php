b0842fae801e27115d0c49153d0a2af814772c6e
ammaribrahim95 <ammaribrahim1995@gmail.com>
2023-07-02 22:06:57 +0800

    add

--- materials/notes/submission/Q8/q4/ammaribrahim95/js/calcTotal.js
+++ materials/notes/submission/Q8/q4/ammaribrahim95/js/calcTotal.js
@@ -1,19 +1,19 @@
 function calcTotal() {
   // Get the input values for each item
-  var item1Price = parseFloat(document.getElementById("item1").value);
-  var item2Price = parseFloat(document.getElementById("item2").value);
-  var item3Price = parseFloat(document.getElementById("item3").value);
-  var item4Price = parseFloat(document.getElementById("item4").value);
-  var item5Price = parseFloat(document.getElementById("item5").value);
+  var Price1 = parseFloat(document.getElementById("item1").value);
+  var Price2 = parseFloat(document.getElementById("item2").value);
+  var Price3 = parseFloat(document.getElementById("item3").value);
+  var Price4 = parseFloat(document.getElementById("item4").value);
+  var Price5 = parseFloat(document.getElementById("item5").value);
 
   // Check if all input fields are filled
-  if (isNaN(item1Price) || isNaN(item2Price) || isNaN(item3Price) || isNaN(item4Price) || isNaN(item5Price)) {
+  if (isNaN(Price1) || isNaN(Price2) || isNaN(Price3) || isNaN(Price4) || isNaN(Price5)) {
     alert("Please enter valid prices for all items.");
     return;
   }
 
   // Calculate the total amount
-  var total = item1Price + item2Price + item3Price + item4Price + item5Price;
+  var total = Price1 + Price2 + Price3 + Price4 + Price5;
 
   // Get a reference to the finalAmount text box element
   var finalAmountTextBox = document.getElementById("finalAmount");
