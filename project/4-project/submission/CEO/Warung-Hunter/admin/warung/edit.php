7ad71d7e525eb88ccb679df943ce08cf45c5cb58
oldpistol <oldpistol@gmail.com>
2023-07-04 23:22:04 +0800

    create and edit functionality

--- project/4-project/submission/CEO/Warung-Hunter/admin/warung/edit.php
+++ project/4-project/submission/CEO/Warung-Hunter/admin/warung/edit.php
@@ -2,9 +2,53 @@
 
 session_start();
 
+require_once "../../helpers/utils.php";
+require_once "../../config/database.php";
+
 if (!isset($_SESSION['is_admin'])) {
     header("Status: 404 Not Found");
-    echo "<h1>404 File not found</h1>";
+    echo "<h1>404 Not found</h1>";
+    exit();
+}
+
+$data = [];
+
+if (isset($_GET['id'])) {
+
+    $id = $_GET['id'];
+
+    try {
+        $sql = "SELECT * FROM stalls WHERE id = '$id'";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows === 1) {
+            $data = mysqli_fetch_assoc($result);
+
+            dd($data);
+        } else {
+            header("Status: 404 Not Found");
+            echo "<h1>404 Not found</h1>";
+            exit();
+        }
+
+        mysqli_close($conn);
+    } catch (Exception $e) {
+        echo "Error: " . $e->getMessage();
+        exit();
+    }
+} else {
+    echo "Not Found.";
+    exit();
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    try {
+        // $sql = "SELECT * FROM stalls WHERE id = "
+        // $result = 
+    } catch (Exception $e) {
+        echo "Error: " . $e->getMessage();
+        exit();
+    }
 }
 
 ?>
