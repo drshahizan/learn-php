60d8a531f4ddeb417e704e398efb5902fc859ad5
badz20 <badrulz2000@gmail.com>
2023-07-08 11:53:05 +0800

    update

---
+++ materials/notes/submission/Q7/q5/badz20/conn.php
@@ -0,0 +1,18 @@
+<?php
+$host = "localhost";
+$username = "admin_persaka";
+$password = "Pers@ka";
+$database = "Persaka";
+
+// Create a connection to the database
+$conn = new mysqli($host, $username, $password, $database);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// If the connection is successful, you can perform database operations here.
+
+// Close the database connection when you're done.
+$conn->close();
