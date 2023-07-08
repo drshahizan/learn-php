36777fd16ffd4ee6b6cd954033a0f77df72257dc
MOHD NOOR HAFIZ BIN MUHAMAD SAID <128025698+mohdnoorhafiz@users.noreply.github.com>
2023-07-08 20:50:49 +0800

    Update form.php

--- materials/notes/submission/Q7/q4/mohdnoorhafiz/form.php
+++ materials/notes/submission/Q7/q4/mohdnoorhafiz/form.php
@@ -1,13 +1,18 @@
 <!DOCTYPE html>
 <html>
-<head> <title>Welcome</title> </head>
+
+<head>
+    <title>Welcome</title>
+</head>
+
 <body>
-<form action = "form.php" method = "POST"> 
- What is your name?<br>
- <input type="text" name="name"><br><br>
- What is your birth year?<br>
- <input type="text" name="number"><br><br>
- <input type="submit" value="Submit">
-</form>
+    <form action="welcome.php" method="POST">
+        What is your name?<br>
+        <input type="text" name="name"><br><br>
+        What is your birth year?<br>
+        <input type="text" name="number"><br><br>
+        <input type="submit" value="Submit">
+    </form>
 </body>
+
 </html>
