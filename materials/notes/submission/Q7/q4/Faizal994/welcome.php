7ebd56ca36af637ceccc10ab7bceebaf32d3d9b9
Faizal994 <128142316+Faizal994@users.noreply.github.com>
2023-07-10 01:01:52 +0800

    Create welcome.php

---
+++ materials/notes/submission/Q7/q4/Faizal994/welcome.php
@@ -0,0 +1,6 @@
+I. Assume the form is written in an HTML file named form.html as shown in Figure 1(c)
+above. The page generated from the server is named welcome.php as shown in Figure
+1(d) above. What should you write at the blank line? [ 1 mark ]
+
+
+<form method="POST" action="welcome.php">
