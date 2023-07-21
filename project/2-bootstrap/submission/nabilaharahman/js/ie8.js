8229477d3719b65cff4185bf2dfe3ac787c6beb6
Mohd Shahizan Othman <shahizan@utm.my>
2023-07-21 11:40:50 +0800

    update

--- project/2-bootstrap/submission/nabilaharahman/js/ie8.js
+++
@@ -1,3 +0,0 @@
-jQuery(document).ready(function(e) {
-    jQuery('body').html('<div class="ie8"><span>Hi! Please, use the latest version of the browser in order to visit current page</span></div>')
-});
